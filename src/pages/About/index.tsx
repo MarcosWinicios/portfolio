8a74bfa99e4454ef654c10745030cea5f1426dec
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-09-21 19:13:47 -0300

    about page styles

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,15 +1,25 @@
+import './styles.css';
+
 const About = () => {
     return (
-        <section id="about">
-            <h1 className="title-page">Sobre</h1>
-            <section className="about-container">
-                {/* <figure className="img-profile">
-                    <img src="#" alt="Marcos Winicios" />
-                </figure> */}
-                <article>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati nostrum ea aliquam est nemo iure laborum rerum eos deserunt aperiam expedita cupiditate illo, velit, perferendis iste exercitationem ab, perspiciatis iusto.
-                </article>
-            </section>
+        <section id="about" className="page">
+            <div className="page-container">
+                <div className="image-profile">
+                    <div className="img"></div>
+                </div>
+                <div className="about-text">
+                    <h1 className="title-page">Sobre</h1>
+                    <article>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati nostrum ea aliquam est nemo iure laborum rerum eos deserunt aperiam expedita cupiditate illo, velit, perferendis iste exercitationem ab, perspiciatis iusto.
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati nostrum ea aliquam est nemo iure laborum rerum eos deserunt aperiam expedita cupiditate illo, velit, perferendis iste exercitationem ab, perspiciatis iusto.
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati nostrum ea aliquam est nemo iure laborum rerum eos deserunt aperiam expedita cupiditate illo, velit, perferendis iste exercitationem ab, perspiciatis iusto.
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati nostrum ea aliquam est nemo iure laborum rerum eos deserunt aperiam expedita cupiditate illo, velit, perferendis iste exercitationem ab, perspiciatis iusto.
+                    </article>
+                </div>
+
+
+            </div>
+
         </section>
 
     );
