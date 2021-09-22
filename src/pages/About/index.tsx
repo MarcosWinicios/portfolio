14fb18b649463b90591f2cffd1334c9c55ba4497
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-09-21 21:47:22 -0300

    add initial image

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -7,7 +7,9 @@ const About = () => {
                 <h1 className="title-page">Sobre</h1>
                 <div className="about-container">
                     <div className="image-profile-container">
-                        <div className="img"></div>
+                        <div className="img">
+                            <img src="https://github.com/marcoswinicios.png" alt="" />
+                        </div>
                     </div>
                     <article>
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati nostrum ea aliquam est nemo iure laborum rerum eos deserunt aperiam expedita cupiditate illo, velit, perferendis iste exercitationem ab, perspiciatis iusto.
