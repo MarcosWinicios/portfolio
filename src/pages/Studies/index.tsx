85a74bcf635c1ca1f41c1d7e52d4f5c240765d81
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-09-24 16:21:33 -0300

    conclused page studies

--- src/pages/Studies/index.tsx
+++ src/pages/Studies/index.tsx
@@ -1,5 +1,7 @@
-import { FiLink } from 'react-icons/fi';
+import { FiLink, FiBook } from 'react-icons/fi';
 import { AiOutlineInfoCircle } from 'react-icons/ai'
+import { FaGraduationCap, FaUniversity, FaRegClock } from 'react-icons/fa';
+
 
 import './styles.css';
 
@@ -10,84 +12,113 @@ const Studies = () => {
                 <h1 className="title-page">Estudos</h1>
                 <div className="course-item">
                     <div className="informations">
-                        <h3>BACHARELADO EM SISTEMAS DE INFORMAÇÃO</h3>
-                        <h4>Institudo Federal Goiano - Campus Ceres</h4>
-                        <p>Status: Cursando</p>
-                        <p>3600 hrs</p>
+                        <div>
+                            <FaGraduationCap />
+                            <h3>BACHARELADO EM SISTEMAS DE INFORMAÇÃO</h3>
+                        </div>
+                        <div>
+                            <FaUniversity />
+                            <h4>Institudo Federal Goiano - Campus Ceres</h4>
+                        </div>
+                        <div>
+                            <FaRegClock />
+                            <p>3600 hrs</p>
+                        </div>
+                        <p>Em andamento</p>
                     </div>
                     <div className="coursers-icons-container">
-                        <a href="https://ifgoiano.edu.br/" target="_blank"  >
-                            <FiLink />
-                        </a>
                         <a href="#" >
                             <AiOutlineInfoCircle />
                         </a>
+                        <a href="https://ifgoiano.edu.br/" target="_blank"  >
+                            <FiLink />
+                        </a>
                     </div>
                 </div>
+
+
                 <div className="course-item">
                     <div className="informations">
-                        <h3>BACHARELADO EM SISTEMAS DE INFORMAÇÃO</h3>
-                        <h4>Institudo Federal Goiano - Campus Ceres</h4>
-                        <p>Status: Cursando</p>
-                        <p>3600 hrs</p>
+                        <div>
+                            <FaGraduationCap />
+                            <h3>INTRODUÇÃO AO GIT E AO GITHUB</h3>
+                        </div>
+                        <div>
+                            <FaUniversity />
+                            <h4>DIGITAL INNOVATION ONE</h4>
+                        </div>
+                        <div>
+                            <FaRegClock />
+                            <p>5 hrs</p>
+                        </div>
+                        <p>Concluído</p>
                     </div>
                     <div className="coursers-icons-container">
-                        <a href="https://ifgoiano.edu.br/" target="_blank"  >
-                            <FiLink />
-                        </a>
                         <a href="#" >
                             <AiOutlineInfoCircle />
                         </a>
+                        <a href="https://digitalinnovation.one/" target="_blank"  >
+                            <FiLink />
+                        </a>
                     </div>
                 </div>
+
                 <div className="course-item">
                     <div className="informations">
-                        <h3>BACHARELADO EM SISTEMAS DE INFORMAÇÃO</h3>
-                        <h4>Institudo Federal Goiano - Campus Ceres</h4>
-                        <p>Status: Cursando</p>
-                        <p>3600 hrs</p>
+                        <div>
+                            <FaGraduationCap />
+                            <h3>BACHARELADO EM SISTEMAS DE INFORMAÇÃO</h3>
+                        </div>
+                        <div>
+                            <FaUniversity />
+                            <h4>Institudo Federal Goiano - Campus Ceres</h4>
+                        </div>
+                        <div>
+                            <FaRegClock />
+                            <p>3600 hrs</p>
+                        </div>
+                        <p>Em andamento</p>
                     </div>
                     <div className="coursers-icons-container">
-                        <a href="https://ifgoiano.edu.br/" target="_blank"  >
-                            <FiLink />
-                        </a>
                         <a href="#" >
                             <AiOutlineInfoCircle />
                         </a>
+                        <a href="https://ifgoiano.edu.br/" target="_blank"  >
+                            <FiLink />
+                        </a>
                     </div>
                 </div>
+
                 <div className="course-item">
                     <div className="informations">
-                        <h3>BACHARELADO EM SISTEMAS DE INFORMAÇÃO</h3>
-                        <h4>Institudo Federal Goiano - Campus Ceres</h4>
-                        <p>Status: Cursando</p>
-                        <p>3600 hrs</p>
+                        <div>
+                            <FaGraduationCap />
+                            <h3>BACHARELADO EM SISTEMAS DE INFORMAÇÃO</h3>
+                        </div>
+                        <div>
+                            <FaUniversity />
+                            <h4>Institudo Federal Goiano - Campus Ceres</h4>
+                        </div>
+                        <div>
+                            <FaRegClock />
+                            <p>3600 hrs</p>
+                        </div>
+                        <p>Em andamento</p>
                     </div>
                     <div className="coursers-icons-container">
+                        <a href="#"  >
+                            <AiOutlineInfoCircle className="icon-info" />
+                        </a>
                         <a href="https://ifgoiano.edu.br/" target="_blank"  >
                             <FiLink />
                         </a>
-                        <a href="#" >
-                            <AiOutlineInfoCircle />
-                        </a>
                     </div>
                 </div>
-                <div className="course-item">
-                    <div className="informations">
-                        <h3>BACHARELADO EM SISTEMAS DE INFORMAÇÃO</h3>
-                        <h4>Institudo Federal Goiano - Campus Ceres</h4>
-                        <p>Status: Cursando</p>
-                        <p>3600 hrs</p>
-                    </div>
-                    <div className="coursers-icons-container">
-                        <a href="https://ifgoiano.edu.br/" target="_blank"  >
-                            <FiLink />
-                        </a>
-                        <a href="#" >
-                            <AiOutlineInfoCircle />
-                        </a>
-                    </div>
-                </div>
+
+
+
+
+
             </div>
         </section>
     );
